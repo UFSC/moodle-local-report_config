21a10235e92b56cd02b0285458a02ad468b71336
luizhsalazar <luizhsalazar@gmail.com>
2015-11-03 12:23:49 -0200

    Alterando busca das atividades dos cursos para apresentar na configuração

--- locallib.php
+++ locallib.php
@@ -21,5 +21,35 @@ function get_activities_courses() {
         }
     }
 
-   return get_atividades_cursos($ids_courses, false, false, false);
+    $assigns = query_assign_courses($ids_courses);
+    $foruns = query_forum_courses($ids_courses);
+    $quizes = query_quiz_courses($ids_courses);
+    $databases = query_database_courses($ids_courses);
+    $scorms = query_scorm_courses($ids_courses);
+
+    $group_array = new GroupArray();
+
+    foreach ($assigns as $atividade) {
+        $group_array->add($atividade->course_id, new report_unasus_assign_activity_config($atividade));
+    }
+
+    foreach ($foruns as $forum) {
+        $group_array->add($forum->course_id, new report_unasus_forum_activity_config($forum));
+    }
+
+    foreach ($quizes as $quiz) {
+        $group_array->add($quiz->course_id, new report_unasus_quiz_activity_config($quiz));
+    }
+
+    foreach ($databases as $database) {
+        $group_array->add($database->course_id, new report_unasus_db_activity_config($database));
+    }
+
+    foreach ($scorms as $scorm) {
+        $group_array->add($scorm->course_id, new report_unasus_scorm_activity_config($scorm));
+    }
+
+    return $group_array->get_assoc();
+
+//   return get_atividades_cursos($ids_courses, false, false, false, true);
 }
