dbb5f1788298e72035cb9d60c2e19d0eee430fdb
luizhsalazar <luizhsalazar@gmail.com>
2015-12-16 19:44:38 -0200

    Alterando estrutura do relatórios para trazer a opção de configurar a atividade de TCC.

--- config_form.php
+++ config_form.php
@@ -35,32 +35,51 @@ class Config_form extends moodleform {
 
         $settings = $DB->get_records('activities_course_config', array('categoryid' => $categoryid));
 
+        $tcc = new stdClass();
+        $tcc->name = 'TCC';
+        $tcc->id = 1;
+
+        $tcc_module = end($atividades_curso);
+        array_push($tcc_module, $tcc);
+
+        array_pop($atividades_curso);
+        array_push($atividades_curso, $tcc_module);
+
         foreach ($atividades_curso as $id_course => $activities){
 
             $i = 0;
 
             foreach ($activities as $activity){
 
+                $mform->addElement('html', '<div>');
+
                 if(!isset($activities_module[$id_course])){
                     $mform->addElement('html', '<h4>');
 
                     $activities_module[$id_course][] = $mform->addElement('html', $activity->course_name);
 
-                    $mform->addElement('html', '<br>');
+                    $mform->addElement('html', '</h4>');
                     $mform->setType('course_name', PARAM_TEXT);
                 }
 
-                if (get_class($activity) == 'report_unasus_lti_activity'){
-                    $name = $activity->position . '-' . $i;
+                /* Estrutura que traz os capítulos dos TCCs para dentro da configuração */
 
-                    /* Os capítulos do TCC tem todos o mesmo id da atividade. Posição do capítulo é usado como índice do array */
-                    $index = $activity->position;
-                } else {
-                    $name = $id_course . '-' . $i;
-                }
+//                if (get_class($activity) == 'report_unasus_lti_activity'){
+//                    $name = $activity->position . '-' . $i;
+//
+//                    /* Os capítulos do TCC tem todos o mesmo id da atividade. Posição do capítulo é usado como índice do array */
+//                    $index = $activity->position;
+//                } else {
+//                    $name = $id_course . '-' . $i;
+//                }
+
+                $name = $id_course . '-' . $i;
+
+                $mform->addElement('html', '</div>');
 
                 $activities_module[$id_course][] = $mform->addElement('checkbox', $name, $activity->name);
                 $mform->setType($name, PARAM_ALPHANUM);
+
 
                 // Se ainda não há configuração para o relatório, o default são todas atividades selecionadas
                 if(empty($settings)) {
@@ -78,15 +97,12 @@ class Config_form extends moodleform {
                 }
 
                 $this->dados[$id_course][$index] = $name;
-                $activities_module[$id_course][] = $mform->addElement('html', '<br>');
 
                 $i++;
             }
 
             $activities_module = '';
         }
-        $mform->addElement('hidden', 'categoryid', $categoryid);
-        $mform->setType('categoryid', PARAM_INT);
 
         $this->add_action_buttons();
     }
