afd7fb381e584a0d853481f137cb0443f5444016
Roberto Silvino <robertosilvino@gmail.com>
2015-12-21 09:05:52 -0200

    atualização de versão

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015100103; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015122101; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->component = 'local_report_config'; // Full name of the plugin (used for diagnostics)
 
 $plugin->maturity  = MATURITY_BETA; // this version's maturity level
